d1224f5bbcf428d18674f41bfb3d02e29502e63c
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-05-09 04:44:12 +0000

    [Finish]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :is_matching_login_user, only: [:edit, :update]
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,5 +19,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
 
 end
